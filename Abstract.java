0e572f596eff0f9711948d6cf758051a64bd2774
rakeshkandhi <rakeshkandi12345.com>
2023-02-20 15:44:11 +0530

    first commit

--- Abstract.java
+++ Abstract.java
@@ -1,17 +1,24 @@
 package crt_classes;
 
 abstract class Bank{
-	abstract int getIntrestRate();
+	abstract double getIntrestRate();
 }
 
 class Citi extends Bank{
-	int getIntrestRate() {
+	double getIntrestRate() {
 		return 7;
 	}
 }
+
 class HSBC extends Bank{
-	int getIntrestRate() {
-		return 6;
+	double getIntrestRate() {
+		return 6.8;
+	}
+}
+
+class SBI extends Bank{
+	double getIntrestRate() {
+		return 6.5;
 	}
 }
 public class Abstract {
@@ -21,6 +28,8 @@ public class Abstract {
 		b =new Citi();
 		System.out.println("Citi intrest rates are "+b.getIntrestRate()+"%");
 		b =new HSBC();
+		System.out.println("HSBC intrest rates are "+b.getIntrestRate()+"%");
+		b =new SBI();
 		System.out.println("HSBC intrest rates are "+b.getIntrestRate()+"%");
 	}
 }
