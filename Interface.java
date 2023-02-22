f7269aefe04edb0a8822155af3507a82607bf856
rakeshkandhi <rakeshkandi12345.com>
2023-02-22 15:48:35 +0530

    Day_6

--- Interface.java
+++ Interface.java
@@ -1,12 +1,35 @@
 package crt_classes;
 
-interface Bank{
+import java.util.Arrays;
+
+interface Bnk {
 	float rateofIntrest();
 }
 
+class PNB implements Bnk {
+	public float rateofIntrest() {
+		return 9.15f;
+	}
+}
+
+class Sc implements Bnk {
+	public float rateofIntrest() {
+		return 7.4f;
+	}
+}
+
 public class Interface {
+	public static void print2d() {
+		int[][] cubes = { { 1, 1 }, { 2, 2 }, { 3, 3 } };
+		System.out.println(Arrays.deepToString(cubes));
+	}
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+//		Bnk b;
+//		b = new PNB();
+//		System.out.println("ROI :- " + b.rateofIntrest());
+//		b = new Sc();
+//		System.out.println("ROI :- " + b.rateofIntrest());
+		print2d();
 	}
 }
