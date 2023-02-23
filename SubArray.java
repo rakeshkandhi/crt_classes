5e0e76766a3a5f96e100fc7054acc8ab0f318701
rakeshkandhi <rakeshkandi12345.com>
2023-02-23 15:44:24 +0530

    Array copy

--- SubArray.java
+++ SubArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class SubArray {
 
 	public static void main(String[] args) {
-		String[] best = { "ICIC", "SBI", "CITi", "YATRA", "HDFC", "CANARA" };
+		String[] best = { "ICIC", "SBI", "CITI", "YATRA", "HDFC", "CANARA" };
 		String[] SBI_CARDS = new String[3];
 //		System.arraycopy(best, 3, SBI_CARDS, 0, 2);
 		System.arraycopy(best, 2, SBI_CARDS, 0, 3);
