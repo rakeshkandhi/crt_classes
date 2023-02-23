929b773e47ecda472fbbc6b413f342077b97ffe6
rakeshkandhi <rakeshkandi12345.com>
2023-02-23 15:27:57 +0530

    Delimiter

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -5,23 +5,24 @@ import java.util.Collections;
 
 public class ArrayDemo {
 	public static void main(String[] args) {
-		Integer[] primes = { 7, 11, 19, 13, 17, 2, 3, 5 };
+		Integer[] prime = { 7, 11, 19, 13, 17, 2, 3, 5 };
 //		System.out.println(Arrays.toString(primes));
-		int i = 0;
+		int i;
+//		int j,k;
 //		Arrays.sort(primes, Collections.reverseOrder());
 //		System.out.println(Arrays.toString(primes));
-		Arrays.sort(primes);
-		System.out.println(Arrays.toString(primes));
-		for  (i =primes.length-1;i>0;i--)
-			System.out.println(primes[i]);
+		Arrays.sort(prime);
+		System.out.println(Arrays.toString(prime));
+		for  (i =prime.length-1;i>0;i--)
+			System.out.println(prime[i]);
 
 //		int [][] a = {{1,2,3},{1,2,3},{1,2,3}};
 //		int [][] b = {{1,2,3},{1,2,3},{1,2,3}};
 //		int [][] c = new int[3][3];
-//		for (int i =0;i<3;i++) {
-//			for(int j=0;j<3;j++) {
+//		for (i =0;i<3;i++) {
+//			for(j=0;j<3;j++) {
 //				c[i][j]=0;
-//				for(int k=0;k<3;k++) {
+//				for(k=0;k<3;k++) {
 //					c[i][j] += a[i][k] * b[k][j];
 //				}
 //				System.out.print(c[i][j]+" ");
---
+++ Delimiter.java
@@ -0,0 +1,27 @@
+package crt_classes;
+
+import java.util.Arrays;
+
+class conv {
+	public String arrayToString(String[] currencies, String delimiter) {
+		String result = "";
+		if (currencies.length > 0) {
+			StringBuilder sb = new StringBuilder();
+			for (String s : currencies) {
+				sb.append(s).append(delimiter);
+				result = sb.deleteCharAt(sb.length() - 1).toString();
+			}
+		}
+		return result;
+	}
+}
+
+public class Delimiter {
+	public static void main(String args[]) {
+		String[] currencies = { "USD", "INR", "AUD", "GBP" };
+		conv c = new conv();
+		System.out.println("Array is : "+Arrays.toString(currencies));
+		String output = c.arrayToString(currencies, ",");
+		System.out.println(output);
+	}
+}
