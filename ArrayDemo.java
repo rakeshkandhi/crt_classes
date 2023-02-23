49b4a1ae5610c3a3657dae7901381db775d55989
rakeshkandhi <rakeshkandi12345.com>
2023-02-23 13:37:15 +0530

    Array

---
+++ ArrayDemo.java
@@ -0,0 +1,10 @@
+package crt_classes;
+
+import java.util.Arrays;
+
+public class ArrayDemo {
+	public static void main(String[] args) {
+		int[] primes= {2,3,5,7,11,13,17,19};
+		System.out.println(Arrays.toString(primes));
+	}
+}
