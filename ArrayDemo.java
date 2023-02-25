2b053c0197ca89d5afdc88798d52b36f6da7f667
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-02-25 21:54:46 +0530

    Dat_7

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -5,16 +5,25 @@ import java.util.Collections;
 
 public class ArrayDemo {
 	public static void main(String[] args) {
-		Integer[] prime = { 7, 11, 19, 13, 17, 2, 3, 5 };
-		System.out.println(Arrays.toString(prime));
-		int i;
+		char a[]= {'a','5','A',' '};
+		System.out.println(Character.isDigit(a[0]));
+		System.out.println(Character.isWhitespace(a[3]));
+		System.out.println(Character.isUpperCase(a[2]));
+	}
+}
+//		byte []a= {65,66,67,68,69,70};
+//		byte []b= {71,72,73,74,75,76};
+//		System.arraycopy(a, 2, b, 1, a.length-2);
+//		Integer[] prime = { 7, 11, 19, 13, 17, 2, 3, 5 };
+//		System.out.println(Arrays.toString(prime));
+//		int i;
 //		int j,k;
 //		Arrays.sort(primes, Collections.reverseOrder());
 //		System.out.println(Arrays.toString(primes));
-		Arrays.sort(prime);
-		System.out.println(Arrays.toString(prime));
-		for  (i =prime.length-1;i>0;i--)
-			System.out.println(prime[i]);
+//Arrays.sort(prime);
+//System.out.println(Arrays.toString(prime));
+//for  (i =prime.length-1;i>0;i--)
+//	System.out.println(prime[i]);
 
 //		int [][] a = {{1,2,3},{1,2,3},{1,2,3}};
 //		int [][] b = {{1,2,3},{1,2,3},{1,2,3}};
@@ -29,8 +38,6 @@ public class ArrayDemo {
 //			}
 //			System.out.println();
 //		}
-		String s[] = { "apple", "banana", "mango" };
-		Arrays.sort(s, Collections.reverseOrder());
-		System.out.println(Arrays.toString(s));
-	}
-}
+//		String s[] = { "apple", "banana", "mango" };
+//		Arrays.sort(s, Collections.reverseOrder());
+//		System.out.println(Arrays.toString(s));
