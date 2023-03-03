3a588424e3ab4d15c6cf6d66b3e3fc5cbf70bb24
rakeshkandhi <rakeshkandi12345@gmail.com>
2023-03-03 23:08:05 +0530

    Threading

--- CommandLineArgs.java
+++ CommandLineArgs.java
@@ -1,12 +1,34 @@
 package crt_classes;
 
+//import java.awt.*;
 import java.util.Arrays;
-
-public class CommandLineArgs {
+public class CommandLineArgs extends Thread {
 
 	public static void main(String[] args) {
 //		for (int i = 0; i < args.length; i++)
 //			System.out.println(args[i]);
 		System.out.println(Arrays.toString(args));
+		Thread t1 = Thread.currentThread();
+		System.out.println(t1.isAlive());
+		Thread t = Thread.currentThread();
+		t.setName("New Thread");
+		System.out.println(t.getName());
+		new newthread();
+	}
+}
+
+class newthread implements Runnable {
+	Thread t1, t2;
+
+	newthread() {
+		t1 = new Thread(this, "Thread_l");
+		t2 = new Thread(this, "Thread");
+		t1.start();
+		t2.start();
+	}
+
+	public void run() {
+		t2.setPriority(Thread.MAX_PRIORITY);
+		System.out.println(t1.equals(t2));
 	}
 }
